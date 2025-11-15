12ae04e4a0efcbba5e33d11bfa5d9711c8fe2a98
musfirah <225047960+mimirock909@users.noreply.github.com>
2025-11-15 00:28:47 -0800

    Add files via upload

---
+++ leetcode/climbing-stairs.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int climbStairs(int n) {
+	if(n==1){
+		return 1;
+	}
+	if(n==2){
+		return 2;
+	}
+	
+	return(climbStairs(n-1)+climbStairs(n-2));
+ 
+}
+int main(){
+	int n;
+	scanf("%d",&n);
+    printf("%d",climbStairs(n));
+}
